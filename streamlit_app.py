21bdf310ddcbc0dcbefe7e21e0c757d9edeb9a3f
Ila26 <102522612+Ila26@users.noreply.github.com>
2023-07-29 19:48:28 +0530

    Update streamlit_app.py
    Added text

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,2 +1,8 @@
 import streamlit
-streamlit.title('My parents new healthy diner')
+
+streamlit.title('My Parents New Healthy diner')
+
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & blueberry oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')

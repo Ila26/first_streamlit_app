5d1d340df1c1543bad83aa24e4514665dce31fce
Ila26 <102522612+Ila26@users.noreply.github.com>
2023-07-29 20:00:53 +0530

    Update streamlit_app.py
    Header update

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@ import streamlit
 
 streamlit.title('My Mom's New Healthy diner')
 
-streamlit.header('Breakfast Menu')
+streamlit.header('Breakfast Favorites')
 streamlit.text('🥣 Omega 3 & blueberry oatmeal')
 streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
 streamlit.text('🐔 Hard-Boiled Free-Range Egg')
